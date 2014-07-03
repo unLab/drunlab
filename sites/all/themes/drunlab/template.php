c82657b334ed074fa0ee54a658313e0df6cf665a
Mike Gossmann <mike@c572.ca>
2014-07-03 01:28:21 +0000

    Starts styling the main menu

--- sites/all/themes/drunlab/template.php
+++ sites/all/themes/drunlab/template.php
@@ -5,6 +5,14 @@
  */
 
 /**
+ * Implements hook_css_alter().
+ */
+function drunlab_css_alter(&$css) {
+  $system = drupal_get_path('module', 'system');
+  unset($css[$system . '/system.menus.css']);
+}
+
+/**
  * Implements theme_panels_default_style_render_region().
  */
 function drunlab_panels_default_style_render_region($vars) {
